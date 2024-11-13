7175903c6085e44e3b180d7149e106bbc7231894
CardenasJesus <cardenaszalapajesus@gmail.com>
2024-11-13 12:39:39 -0800

    plantilla sidebar

--- resources/js/Pages/Contracts/showContract.vue
+++ resources/js/Pages/Contracts/showContract.vue
@@ -5,6 +5,8 @@ import { Head } from '@inertiajs/vue3';
 
 const Contracts = [
     {status: 'Nuevo', beingDate: '19-01-2023',endDate: '10-10-2025', tenant: 'Juan Manuel Lopez Uribe', owner: 'Derian Tarango', mount: '$5,000', type: 'Departamento', address: 'Calle 123 # 123-123', city: 'Tijuana', state: 'Baja California', country: 'Mexico'},
+    {status: 'Nuevo', beingDate: '19-01-2023',endDate: '10-10-2025', tenant: 'Juan Manuel Lopez Uribe', owner: 'Derian Tarango', mount: '$5,000', type: 'Departamento', address: 'Calle 123 # 123-123', city: 'Tijuana', state: 'Baja California', country: 'Mexico'},
+    {status: 'Nuevo', beingDate: '19-01-2023',endDate: '10-10-2025', tenant: 'Juan Manuel Lopez Uribe', owner: 'Derian Tarango', mount: '$5,000', type: 'Departamento', address: 'Calle 123 # 123-123', city: 'Tijuana', state: 'Baja California', country: 'Mexico'},
 ];
 
 </script>
@@ -12,8 +14,8 @@ const Contracts = [
 <template>
     <Head title="Contracts" />
         <DashboardLayout>
-            <section class="grid grid-flow-row-dense grid-cols-1 sm:grid-cols-3  grid-rows-1 sm:grid-rows-3  gap-1 sm:gap-8 w-full justify-center items-center text-center pt-8">
-              <nav class="col-span-2 block bg-gray-100 rounded-lg shadow-lg p-2">
+            <section class="grid grid-cols-1 sm:grid-cols-3 gap-1 sm:gap-8 w-full justify-center items-center text-center pt-8">
+              <nav class="col-span-2 block bg-gray-100 rounded-lg shadow-lg p-2 h-[550px]">
                 <div class="flex justify-between items-center p-2 text-grayt-200">
                     <h1 class="text-lg font-bold w-full flex">Contract Details</h1>
                 </div>
@@ -21,12 +23,34 @@ const Contracts = [
 
                 </div>
               </nav>
-              <nav class="block bg-gray-100 rounded-lg shadow-lg p-2">
+              <nav class="block bg-gray-100 rounded-lg shadow-lg p-2 ">
                 <div class="flex justify-between items-center p-2">
                     <h1 class="text-lg font-bold w-full flex">All Contracts</h1>
                 </div>
+                <div class="block justify-center text-start p-2  overflow-y-auto h-[500px]">
+                    <div v-for="Contract in Contracts" class="bg-white rounded-lg shadow-lg p-2 m-2">
+                        <nav class="block sm:flex justify-between py-2">
+                            <h1 class="text-sm text-wrap text-black font-black"> {{ Contract.type }}</h1>
+                            <h1 class="text-sm text-pretty text-primary-black font-black"> {{ Contract.mount }}</h1>
+                        </nav>
+                        <span class="text-sm font-bold text-start underline">Address:</span><p class="text-xs"> {{ Contract.address }}, {{ Contract.city }} {{ Contract.state }}</p>
+                        <span class="text-sm font-bold text-start underline">Tenant:</span><p class="text-xs"> {{ Contract.tenant }}</p>
+                        <span class="text-sm font-bold text-start underline">Owner:</span><p class="text-xs"> {{ Contract.owner }}</p>
+
+                        <div class="grid grid-cols-1 sm:grid-cols-3 gap-2 justify-center items-center text-center ">
+                            <div class="items-center">
+                                <span class="text-sm font-bold text-start underline">Status:</span><p class="text-xs"> {{ Contract.status }}</p>
+                            </div>
+                            <div class="items-center">
+                                <span class="text-sm font-bold text-start underline">Being Date:</span><p class="text-xs"> {{ Contract.beingDate }}</p>
+                            </div>
+                            <div class="items-center">
+                                <span class="text-sm font-bold text-start underline">End Date:</span><p class="text-xs"> {{ Contract.endDate }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
               </nav>
-            
             </section>
         <!-- <div class="w-full justify-center items-center p-8">
             <table class="table-auto w-full">
