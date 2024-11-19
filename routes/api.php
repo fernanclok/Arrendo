52e03e2d63a14565625525d8395c2e8fb60bc677
Fernando Medina <fernanclok@gmail.com>
2024-11-19 14:13:18 -0800

    hf

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContractController;
 
-
 use App\Http\Controllers\ZoneController;
-use App\Http\Controllers\PropertyController;
-
 use App\Http\Controllers\PropertyController;
 
 /*
