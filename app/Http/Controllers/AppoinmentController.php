c8455fb26d533e9ffd2c02c39ce75c29ecb5fe20
Fernando Medina <fernanclok@gmail.com>
2024-11-26 09:57:27 -0800

    feat: added appoinments and notifications

--- app/Http/Controllers/AppoinmentController.php
+++ app/Http/Controllers/AppoinmentController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Appoinment;
 
 class AppoinmentController extends Controller
 {
-    //
+    public function createAppoinment(Request $request)
+    {
+        $request->validate([
+            'property_id' => 'required',
+            'tenant_user_id' => 'required',
+            'requested_date' => 'required',
+            'status' => 'required', 
+        ]);
+
+        $appoinment = new Appoinment();
+        $appoinment->property_id = $request->property_id;
+        $appoinment->user_id = $request->tenant_user_id;
+        $appoinment->requested_date = $request->requested_date;
+        $appoinment->status = $request->status;
+        $appoinment->save();
+
+        return response()->json([
+            'message' => 'Appoinment created successfully',
+            'data' => $appoinment
+        ]);
+    }
 }
