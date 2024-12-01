4a3441ef9a7e13d516d98f7edf818e31391c6c1d
Fernando Medina <fernanclok@gmail.com>
2024-12-01 14:46:38 -0800

    hf

--- resources/js/Pages/invoice/MyInvoices.vue
+++ resources/js/Pages/invoice/MyInvoices.vue
@@ -6,7 +6,6 @@ import { Head, usePage } from '@inertiajs/vue3';
 
 const user = usePage().props.auth.user;
 const invoices = ref([]);
-
 // Muestra los recibos por usuario
 const fetchInvoices = async () => {
     try {
@@ -21,24 +20,16 @@ const fetchInvoices = async () => {
     }
 };
 
-const generatePDF = async (invoiceId) => {
-    try {
-        const response = await axios.get(`/api/my-invoices/${invoiceId}/pdf`, {
-            responseType: 'blob',
-        });
 
-        // URL del Archivo descargado
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', `invoice-${invoiceId}.pdf`);
-        document.body.appendChild(link);
-        link.click();
+//Funcion para pagar
+const InvoicePaid = async (invoiceId) => {
+    try {
+        await axios.patch(`/api/invoices/${invoiceId}/invoice-paid`);
+        fetchInvoices();
     } catch (error) {
-        console.error('Error generating PDF:', error);
+        console.error('Error marking invoice as paid:', error);
     }
 };
-
 
 onMounted(fetchInvoices);
 </script>
@@ -47,7 +38,6 @@ onMounted(fetchInvoices);
   <Head title="My Invoices" />
   <DashboardLayout>
     <div class="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
-      <h1 class="text-2xl font-semibold text-gray-900">My Invoices Tenant</h1>
       <table class="w-full table-fixed">
         <thead>
           <tr class="bg-gray-100">
@@ -62,20 +52,20 @@ onMounted(fetchInvoices);
           </tr>
         </thead>
         <tbody class="bg-white">
-          <tr v-for="(invoice, index) in invoices" :key="invoice.id">
+          <tr v-for="(invoice, index) in invoices" :key="invoice.invoice_id">
             <td class="py-4 px-6">{{ index + 1 }}</td>
             <td class="py-4 px-6">{{ invoice.invoice_date }}</td>
             <td class="py-4 px-6">{{ invoice.contract_id }}</td>
-            <td class="py-4 px-6">{{ invoice.user_id }}</td>
+            <td class="py-4 px-6">{{ invoice.tenant_name }}</td>
             <td class="py-4 px-6">{{ invoice.issue_date }}</td>
             <td class="py-4 px-6">{{ invoice.total_amount }}</td>
             <td class="py-4 px-6">{{ invoice.payment_status }}</td>
             <td class="py-4 px-6">
               <button 
-                @click="generatePDF(invoice.id)" 
+                @click="InvoicePaid(invoice.id)" 
                 class="bg-green-500 text-white px-4 py-2 rounded"
               >
-                Generar Pdf
+                Pagado
               </button>
             </td>
           </tr>
